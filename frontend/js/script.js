d94c1c6ea9160227888444d0cacf6f388f2b1628
dtelega <dima.hannasay@gmail.com>
2018-09-06 19:30:56 +0300

    add some logic to front

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -56,7 +56,6 @@ var urlImageVersion = 'https://cisco-presence.unit.ua/api/config/v1/version/imag
 var urlAllUser = 'https://cisco-presence.unit.ua/api/config/v1/aaa/users';
 
 var floorUrl = 'https://cisco-cmx.unit.ua/api/analytics/v1/now/clientCount';
-var visitorsYesterdayUrl = 'https://cisco-cmx.unit.ua/api/presence/v1/connected/count/yesterday';
 
 var imageUrl1 = 'https://cisco-cmx.unit.ua/api/config/v1/maps/image/System%20Campus/UNIT.Factory/1st_Floor';
 var imageUrl2 = 'https://cisco-cmx.unit.ua/api/config/v1/maps/image/System%20Campus/UNIT.Factory/2nd_Floor';
@@ -73,6 +72,7 @@ var imageUrl3 = 'https://cisco-cmx.unit.ua/api/config/v1/maps/image/System%20Cam
             });
         }
     );
+
 $('#1st_Floor_btn').on('click', function(){
     sendRequest(
         imageUrl1,
@@ -80,15 +80,18 @@ $('#1st_Floor_btn').on('click', function(){
         null,
         function () {
             console.log("success download floor image");
-            $(".img_floor1").html('<img  width="500px" src="' + imageUrl1 + '" />');
+            $(".img_floor1").html('<img  width="100%" src="' + imageUrl1 + '" />');
         }
     );
     $(".img_floor1").show();
     $(".img_floor2").hide();
     $(".img_floor3").hide();
 
-});
 
+    $("#active_user_count").hide();
+    $("#floor_map").show();
+
+});
 $('#2nd_Floor_btn').on('click', function() {
     sendRequest(
         imageUrl2,
@@ -96,14 +99,16 @@ $('#2nd_Floor_btn').on('click', function() {
         null,
         function () {
             console.log("success download floor image");
-            $(".img_floor2").html('<img  width="500px" src="' + imageUrl2 + '" />');
+            $(".img_floor2").html('<img  width="100%" src="' + imageUrl2 + '" />');
         }
     );
     $(".img_floor1").hide();
     $(".img_floor2").show();
     $(".img_floor3").hide();
-});
 
+    $("#active_user_count").hide();
+    $("#floor_map").show();
+});
 $('#3rd_Floor_btn').on('click', function() {
     sendRequest(
         imageUrl3,
@@ -111,40 +116,31 @@ $('#3rd_Floor_btn').on('click', function() {
         null,
         function () {
             console.log("success download floor image");
-            $(".img_floor3").html('<img width="500px" src="' + imageUrl3 + '" />');
+            $(".img_floor3").html('<img width="100%" src="' + imageUrl3 + '" />');
         }
     );
     $(".img_floor1").hide();
     $(".img_floor2").hide();
     $(".img_floor3").show();
+
+    $("#active_user_count").hide();
+    $("#floor_map").show();
 });
 
-function blobToFile(theBlob, fileName){
-    //A Blob() is almost a File() - it just missing the two properties below which we will add
-    theBlob.lastModifiedDate = new Date();
-    theBlob.name = fileName;
-    return theBlob;
-}
+$('#active_user_count_btn').on('click', function () {
+    $("#active_user_count").show();
+    $("#floor_map").hide();
+});
 
 
-/*
- *      File to base64
- * */
-
-function getBase64(file) {
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function () {
-        console.log(reader.result);
-    };
-    reader.onerror = function (error) {
-        console.log('Error: ', error);
-    };
-}
-
-
+// $('#1st_Floor_btn').on('click'), function () {
+//     $("#active_user_count").hide();
+//     $("#floor_map").show();
+// }
 
 //}
+
+
 
 
 function sendRequest(url, type, payload, success) {
