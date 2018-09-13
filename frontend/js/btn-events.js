36d26e2a355b70334f3a15c6947aa01755c1acc9
dtelega <dima.hannasay@gmail.com>
2018-09-13 20:09:10 +0300

    split js to files

---
+++ frontend/js/btn-events.js
@@ -0,0 +1,87 @@
+
+$('#active_user_count_btn').on('click', function () {
+
+    $("#active_user_count").show();
+    $("#floor_map").hide();
+    $("#presence").hide();
+    $("#correlation").hide();
+
+});
+
+$('#1st_Floor_btn').on('click', function () {
+
+    sendRequest(
+        imageUrl+"1st_Floor",
+        password,
+        'GET',
+        null,
+        function () {
+            console.log("success download floor image");
+            $(".img_floor1").html('<img  width="100%" src="' + imageUrl+"1st_Floor" + '" />');
+        }
+    );
+    $(".img_floor1").show();
+    $(".img_floor2").hide();
+    $(".img_floor3").hide();
+
+
+    $("#active_user_count").hide();
+    $("#floor_map").show();
+    $("#presence").hide();
+    $("#correlation").hide();
+
+});
+$('#2nd_Floor_btn').on('click', function () {
+    sendRequest(
+        imageUrl+"2nd_Floor",
+        password,
+        'GET',
+        null,
+        function () {
+            console.log("success download floor image");
+            $(".img_floor2").html('<img  width="100%" src="' + imageUrl+"2nd_Floor" + '" />');
+        }
+    );
+    $(".img_floor1").hide();
+    $(".img_floor2").show();
+    $(".img_floor3").hide();
+
+    $("#active_user_count").hide();
+    $("#floor_map").show();
+    $("#presence").hide();
+    $("#correlation").hide();
+});
+$('#3rd_Floor_btn').on('click', function () {
+    sendRequest(
+        imageUrl+"3rd_Floor",
+        password,
+        'GET',
+        null,
+        function () {
+            console.log("success download floor image");
+            $(".img_floor3").html('<img width="100%" src="' + imageUrl+"3rd_Floor" + '" />');
+        }
+    );
+    $(".img_floor1").hide();
+    $(".img_floor2").hide();
+    $(".img_floor3").show();
+
+    $("#active_user_count").hide();
+    $("#floor_map").show();
+    $("#presence").hide();
+    $("#correlation").hide();
+});
+
+$('#presence_btn').on('click', function () {
+    $("#active_user_count").hide();
+    $("#floor_map").hide();
+    $("#presence").show();
+    $("#correlation").hide();
+});
+
+$('#correlation_btn').on('click', function () {
+    $("#active_user_count").hide();
+    $("#floor_map").hide();
+    $("#presence").hide();
+    $("#correlation").show();
+});
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,88 +1,5 @@
 
 
-// Load the Visualization API and the corechart package.
-
-google.charts.load('current', {'packages':['corechart', 'bar']});
-
-// Set a callback to run when the Google Visualization API is loaded.
-
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-function drawChartTotalUsers(data) {
-    console.log(data);
-    // Create the data table.
-    var chartData = new google.visualization.DataTable();
-    chartData.addColumn('string', 'Floor');
-    chartData.addColumn('number', 'UserCount');
-    $(data.data).each(
-        function (idx, floorInfo) {
-            if (floorInfo.floorName.indexOf('Floor') !== - 1) {
-                //console.log(floorInfo);
-                chartData.addRow([floorInfo.floorName, floorInfo.value]);
-            }
-        }
-    );
-
-    // Set chart options
-    var options = {
-        'title': 'Total active user count = '+ data.total.total,
-        'width': 1200,
-        'height': 700
-    };
-
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-    chart.draw(chartData, options);
-}
-
-function drawHourlyGraph(data, type) {
-    console.log(data);
-
-        var chartData = new google.visualization.DataTable();
-    chartData.addColumn('timeofday', 'Time of Day');
-    if (type === "hourly/today")
-        chartData.addColumn('number', 'Today');
-    else if (type === "hourly/yesterday")
-        chartData.addColumn('number', 'Yesterday');
-    else if (type === "hourly") {
-        chartData.addColumn('number', $("#dravHourlyDate").val()+'');
-    }
-    else if (type === "hourly/3days") { // TODO: make multichart great again
-        chartData.addColumn('number', '1');
-        chartData.addColumn('number', '2');
-        chartData.addColumn('number', '3');
-    }
-
-    for (var i = 0; i < 25; i++) {
-        chartData.addRow(
-            [{v: [i, 0, 0], f: i+""}, data[i]]
-        );
-    }
-
-
-    var options = {
-        title: 'Title',
-        isStacked: true,
-        hAxis: {
-            title: 'Time of Day',
-            format: 'h:mm a',
-            viewWindow: {
-                min: [0, 0, 0],
-                max: [24, 0, 0]
-            }
-        },
-        vAxis: {
-            title: 'Count of connected visitors',
-            'height': 700,
-            'width' : 3000
-        }
-    };
-
-    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div2'));
-    chart.draw(chartData, options);
-
-}
 
 //
 var siteid; // 1513804707441
@@ -113,10 +30,7 @@ password2 = 'Passw0rd';
 
 const floorUrl = 'https://cisco-cmx.unit.ua/api/analytics/v1/now/clientCount';
 
-const imageUrl1 = 'https://cisco-cmx.unit.ua/api/config/v1/maps/image/System%20Campus/UNIT.Factory/1st_Floor';
-const imageUrl2 = 'https://cisco-cmx.unit.ua/api/config/v1/maps/image/System%20Campus/UNIT.Factory/2nd_Floor';
-const imageUrl3 = 'https://cisco-cmx.unit.ua/api/config/v1/maps/image/System%20Campus/UNIT.Factory/3rd_Floor';
-
+const imageUrl = 'https://cisco-cmx.unit.ua/api/config/v1/maps/image/System%20Campus/UNIT.Factory/';
 
 const siteInfoUrl = 'https://cisco-presence.unit.ua/api/config/v1/sites';
 
